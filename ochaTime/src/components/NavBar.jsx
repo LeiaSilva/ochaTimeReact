8f6832d53378c346a5ebce9d3050130e42d41d31
Leia Silva <leiabelensilva@gmail.com>
2026-05-28 13:18:28 -0300

    estilos en navbar

--- ochaTime/src/components/NavBar.jsx
+++ ochaTime/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './NavBar.css';
 
 export const NavBar = ({ carrito }) => {
@@ -10,9 +10,15 @@ export const NavBar = ({ carrito }) => {
                     <Link to={`/`}><p>Ocha Time</p></Link>
                 </div>
                 <ul className="-list">
-                    <Link to={`/Home`}><li>Home</li></Link>
-                    <Link to={`/`}><li>Productos</li></Link>
-                    <Link to={`/QuienesSomos`}><li>Quienes Somos</li></Link>
+                    <li>
+                        <NavLink to={`/Home`} className={({ isActive }) => isActive ? 'activo' : ''}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/`} className={({ isActive }) => isActive ? 'activo' : ''}>Productos</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={`/QuienesSomos`} className={({ isActive }) => isActive ? 'activo' : ''}>Quienes Somos</NavLink>
+                    </li>
                 </ul>
                 <div className="containerCarrito">
                     <span className="-contador">{carrito.length}</span>
