167487ac203d37c13086f6155729651a18671ab0
Leia Silva <leiabelensilva@gmail.com>
2026-07-13 18:39:22 -0300

    responsive en vista admin

--- ochaTime/src/components/NavBarAdmin.jsx
+++ ochaTime/src/components/NavBarAdmin.jsx
@@ -3,15 +3,20 @@ import { useState, useEffect } from 'react';
 import './NavBar.css';
 
 export const NavBarAdmin = () => {
+        const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
             <nav className="nav">
-                <ion-icon name="home-outline" className="homeResponsive"></ion-icon>
+                <ion-icon
+                    name={menuOpen ? "close-outline" : "home-outline"}
+                    className="homeResponsive"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                ></ion-icon>
                 <div className="-logo">
                     <Link to={`/`}><p>Ocha Time</p></Link>
                 </div>
-                <ul className="-list">
+                <ul className={`-list ${menuOpen ? "menuOpen" : ""}`}>
                     <li>
                         <NavLink to={`/ListaProductos`} className={({ isActive }) => isActive ? 'activo' : ''}>Mis Productos</NavLink>
                     </li>
